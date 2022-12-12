05c3aaf09815439b7b1eb15e848e26d60af212ce
MariaOrtizRiera <mortrie@etsinf.upv.es>
2022-12-12 16:22:29 +0100

    Cursor buttons

--- Assets/Scripts/UI/OptionsMenu.cs
+++ Assets/Scripts/UI/OptionsMenu.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class OptionsMenu : MonoBehaviour
 {
+    //public enum SplashStates {}
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +16,10 @@ public class OptionsMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.KeypadEnter))
+        {
+            Debug.Log("Return key was pressed.");
+        }
 
-    }
-
-    public void OnClickBack()
-    {
-        SceneManager.LoadScene("Options");
     }
 }
